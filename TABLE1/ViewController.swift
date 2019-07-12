d58b5e973e337a59ad33294b089995737442d399
Olivia <oliviamirascian@gmail.com>
2019-07-12 11:44:27 -0700

    Initial Commit

---
+++ TABLE1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TABLE1
+//
+//  Created by Olivia Mirascian on 7/12/19.
+//  Copyright © 2019 Olivia Mirascian. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ TABLE1Tests/TABLE1Tests.swift
@@ -0,0 +1,34 @@
+//
+//  TABLE1Tests.swift
+//  TABLE1Tests
+//
+//  Created by Olivia Mirascian on 7/12/19.
+//  Copyright © 2019 Olivia Mirascian. All rights reserved.
+//
+
+import XCTest
+@testable import TABLE1
+
+class TABLE1Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
